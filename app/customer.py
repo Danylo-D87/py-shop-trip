96dbf073d34829b485c3a05166f06f3796e237bd
Danylo-D87 <quaestor88@gmail.com>
2025-05-03 19:59:38 +0300

    sol 2

--- app/customer.py
+++ app/customer.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 import math
 
 from app.car import Car
-
+import app.shop
 
 @dataclass
 class Customer:
@@ -24,16 +24,13 @@ class Customer:
         res = (self.car.fuel_cost(distance, fuel_price))
         return res
 
-    def can_afford_trip(self,
-                        shop_location: list[int],
-                        fuel_price: float,
-                        shop_name: str) -> bool:
+    def can_afford_trip(
+            self,
+            shop: app.shop.Shop,
+            fuel_price: float
+    ) -> bool:
+        cost_product_cart = shop.calculate_product_cart_cost(self.product_cart)
+        cost_fuel = self.calculate_trip_cost(shop.location, fuel_price) * 2
 
-        cost_product_cart = shop_name.calculate_product_cart_cost(
-            self.product_cart)
-        cost_fuel = self.calculate_trip_cost(shop_location, fuel_price)
+        return (cost_fuel + cost_product_cart) <= self.money
 
-        if cost_fuel + cost_product_cart <= self.money:
-            return True
-        else:
-            return False
